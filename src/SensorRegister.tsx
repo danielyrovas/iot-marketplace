4423f12cd82d06ef9d0b6b15ffb05f57cf1c693b
joelwyn <100537283+joelwyn@users.noreply.github.com>
2023-04-23 16:04:02 +1000

    added sensor registration field holding data to be submitted to api

--- src/SensorRegister.tsx
+++ src/SensorRegister.tsx
@@ -1,15 +1,49 @@
-import { createSignal } from "solid-js";
+import { createStore } from 'solid-js/store';
+import { Component } from 'solid-js/web';
 
-export default function SensorRegister() {
+interface FormValues {
+  sensorName: string;
+  sensorType: string;
+  sensorLocation: string;
+}
+
+export default function RegistrationForm(): Component {
+  // create store to hold submitted value
+  const store = createStore<FormValues>({
+    sensorName: '',
+    sensorType: '',
+    sensorLocation: '',
+  });
+
+  // Handle form submission
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+    // Access the form data from the store object directly
+    const { sensorName, sensorType, sensorLocation } = store;
+    // log 
+    console.log('Form submitted with data:', sensorName, sensorType, sensorLocation);
+    // Reset the form by setting the store properties to empty values
+    store.sensorName = '';
+    store.sensorType = '';
+    store.sensorLocation = '';
+  };
+
   return (
-    <div>
-      <h1> Welcome to SENSOR! </h1>
-      <button
-        type="button"
-        onClick={() => (window.location.pathname = "/register/broker")}
-      >
-        Greet
-      </button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>IoT Sensor Registration</h1>
+      <label>
+        <p>Sensor Name:</p>
+        <input type="text" value={" "} onInput={(e: Event) => (store.sensorName = e.target.value)} />
+      </label>
+      <label>
+        <p>Sensor Type:</p>
+        <input type="text" value={" "} onInput={(e: Event) => (store.sensorType = e.target.value)} />
+      </label>
+      <label>
+        <p>Sensor Location:</p>
+        <input type="text" value={" "} onInput={(e: Event) => (store.sensorLocation = e.target.value)} />
+      </label>
+      <p><button type="submit">Register</button></p>
+    </form>
   );
 }
