221150cc7f0f7e21b9352f9e743c27bc930fd2c6
Daniel Yrovas <dan@danielyrovas.com>
2023-04-25 18:27:07 +1000

    add material ui component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { For, lazy } from "solid-js";
+import { Button } from "@suid/material";
 import { A, useRoutes } from "@solidjs/router";
 import "./index.css";
 
@@ -26,16 +27,16 @@ const routes = [
 ];
 export default function App() {
   const Routes = useRoutes(routes);
-  const cardStyle =
-    "h-[160px] p-[5px] m-[20px] aspect aspect-[2] rounded-[16px] shadow-[0_0_0_4px_hsl(0_0%_0%_/_15%)]";
+  // const cardStyle =
+  //   "h-[160px] p-[5px] m-[20px] aspect aspect-[2] rounded-[16px] shadow-[0_0_0_4px_hsl(0_0%_0%_/_15%)]";
   return (
     <>
       <h1>Internet of Things Marketplace</h1>
       <nav class="place-items-center m-[10px]">
         <For each={Object.values(routes || {})}>
           {(a) => (
-            <A class={cardStyle} href={a.path}>
-              {a.name}
+            <A href={a.path}>
+              <Button>{a.name}</Button>
             </A>
           )}
         </For>
