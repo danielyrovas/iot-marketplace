60d5d1c150360ced02f6e3cfa595904eaa044b5e
joelwyn <100537283+joelwyn@users.noreply.github.com>
2023-08-29 21:34:03 +1000

    query done

--- src/sparlql-client.tsx
+++ src/sparlql-client.tsx
@@ -2,25 +2,38 @@ import { createSignal } from 'solid-js';
 import * as jsonld from 'jsonld';
 import { QueryInput } from './QueryInput';
 import { RdfDataDisplay } from './RdfDataDisplay';
-
+import { N3Parser } from 'rdflib';
+import * as rdf from 'rdflib';
+import { json } from 'stream/consumers';
 const sQL = () => {
   const [rdfData, setRdfData] = createSignal<any[]>([]);
 
+
   const fetchRdfData = async (query: string) => {
     try {
-      const response = await fetch('https://query.wikidata.org/sparql', {
-        method: 'POST',
-        headers: {'Accept': 'application/sparql-results+json'
-          
-        },
-        body: query,
-      });
+        console.log(query);
+      const encodedQuery = encodeURIComponent(query);
+      const endpointurl = 'https://query.wikidata.org/sparql';
+      const headers = { 'Accept': 'application/sparql-results+json' };
+      const fullUrl = endpointurl + '?query=' + encodeURIComponent( query );
+      const response =  fetch( fullUrl, { headers } ).then( body => body.json() );
+  
+    //   if (!response.ok) {
+    //     throw new Error(`HTTP error ${response.status}`);
+    //   }
+      const rdfString =  response;
+      console.log(rdfString);
+    //   const abc = response.body;
+      
+    // fetchRdfData(query ).then(response => {console.log(JSON.stringify(rdfString, null, 2))});
 
-      const rdfString = await response.text();
+      
 
-      const jsonldData = await jsonld.fromRDF(rdfString, { format: 'application/n-quads' });
+      // Convert RDF data to JSON-LD format
+    //   const jsonldData = await jsonld.fromRDF(rdfString, { format: 'application/n-quads' });
 
-      setRdfData(jsonldData);
+      setRdfData(await rdfString);
+    //   console.log(jsonldData);
     } catch (error) {
       console.error('Error fetching RDF data:', error);
     }
@@ -29,6 +42,7 @@ const sQL = () => {
   return (
     <div>
       <h1>RDF Data Viewer</h1>
+        <p></p>
       <RdfDataDisplay rdfData={rdfData()} />
       <QueryInput executeQuery={fetchRdfData} />
     </div>
