5ffdf590187044cb54e5960169bf4cbebc2edb17
joelwyn <100537283+joelwyn@users.noreply.github.com>
2023-09-12 21:28:20 +1000

    box update

--- src/logic/QueryInput.jsx
+++ src/logic/QueryInput.jsx
@@ -1,130 +1,129 @@
 import { createSignal } from 'solid-js';
 
 const presetQueries = [
-    {
-        label: 'Person',
-        property: 'P31',
-        value: 'Q5',
-    },
-    {
-        label: 'City',
-        property: 'P31',
-        value: 'Q515',
-    },
-    {
-        label: 'Country',
-        property: 'P31',
-        value: 'Q6256',
-    },
-    // Additional properties
-    {
-        label: 'Occupation',
-        property: 'P106',
-        value: 'Q937857',
-    },
-    {
-        label: 'Genre',
-        property: 'P136',
-        value: 'Q188442',
-    },
-    {
-        label: 'Founder',
-        property: 'P112',
-        value: 'Q937',
-    },
-    // Add more properties here
+  {
+    label: 'Person',
+    property: 'P31',
+    value: 'Q5',
+  },
+  {
+    label: 'City',
+    property: 'P31',
+    value: 'Q515',
+  },
+  {
+    label: 'Country',
+    property: 'P31',
+    value: 'Q6256',
+  },
+  {
+    label: 'Occupation',
+    property: 'P106',
+    value: 'Q937857',
+  },
+  {
+    label: 'Genre',
+    property: 'P136',
+    value: 'Q188442',
+  },
+  {
+    label: 'Founder',
+    property: 'P112',
+    value: 'Q937',
+  },
 ];
 
 export const QueryInput = (props) => {
-    const [selectedQuery, setSelectedQuery] = createSignal(0);
-    const [selectedProperty, setSelectedProperty] = createSignal(
-        presetQueries[0].property
-    );
-    const [customQuery, setCustomQuery] = createSignal('');
-    const [customQueryExecuted, setCustomQueryExecuted] = createSignal(false);
+  const [selectedQuery, setSelectedQuery] = createSignal(0);
+  const [selectedProperty, setSelectedProperty] = createSignal(
+    presetQueries[0].property
+  );
+  const [customQuery, setCustomQuery] = createSignal('');
+  const [loading, setLoading] = createSignal(false); 
 
-    const availableQueryOptions = [];
+  // predicates 
+  const updateAvailableQueryOptions = () => {
+    const availableQueryOptions = presetQueries
+      .filter((preset) => preset.property === selectedProperty())
+      .map((preset, index) => (
+        <option key={index} value={index}>
+          {preset.label}
+        </option>
+      ));
+    return availableQueryOptions;
+  };
 
-    presetQueries.forEach((preset, index) => {
-        if (preset.property === selectedProperty()) {
-            availableQueryOptions.push(
-                <option key={index} value={index}>
-                    {preset.label}
-                </option>
-            );
-        }
-    });
+  const availableQueryOptions = updateAvailableQueryOptions();
 
-    const handleQueryChange = (event) => {
-        const selectedIndex = event.target.value === 'custom' ? 'custom' : parseInt(event.target.value);
-        setSelectedQuery(selectedIndex);
-        setCustomQueryExecuted(false); // Reset custom query execution flag
-    };
+  const handleQueryChange = (event) => {
+    const selectedIndex = parseInt(event.target.value);
+    setSelectedQuery(selectedIndex);
+  };
 
-    const handlePropertyChange = (event) => {
-        const selectedPropertyCode = event.target.value;
-        setSelectedProperty(selectedPropertyCode);
+  const handlePropertyChange = (event) => {
+    const selectedPropertyCode = event.target.value;
+    setSelectedProperty(selectedPropertyCode);
+  };
 
-        availableQueryOptions.length = 0;
-        presetQueries.forEach((preset, index) => {
-            if (preset.property === selectedPropertyCode) {
-                availableQueryOptions.push(
-                    <option key={index} value={index}>
-                        {preset.label}
-                    </option>
-                );
-            }
-        });
-
-        setSelectedQuery(0);
-        setCustomQueryExecuted(false); // Reset custom query execution flag
-    };
-
-    const handlePresetQuerySubmit = (event) => {
-        event.preventDefault();
-        const selectedPresetQuery = `SELECT ?item ?itemLabel WHERE {
+  const handlePresetQuerySubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true); 
+    const selectedPresetQuery = `SELECT ?item ?itemLabel WHERE {
       ?item wdt:${selectedProperty()} wd:${presetQueries[selectedQuery()].value};
       SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
     } LIMIT 10`;
-        props.executeQuery(selectedPresetQuery);
-        setCustomQueryExecuted(false); // Reset custom query execution flag
-    };
+    try {
+      await props.executeQuery(selectedPresetQuery);
+    } finally {
+      setLoading(false); 
+    }
+  };
 
-    const handleCustomQuerySubmit = (event) => {
-        event.preventDefault();
-        props.executeQuery(customQuery());
-        setCustomQueryExecuted(true); // Set custom query execution flag
-    };
+  const handleCustomQuerySubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true); 
+    try {
+      await props.executeQuery(customQuery());
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div>
-            <h2>SPARQL Query</h2>
-            <form onSubmit={handlePresetQuerySubmit}>
-                <select onChange={handleQueryChange}>
-                    {availableQueryOptions}
-                    <option value="custom">Custom Query</option>
-                </select>
-                {/* Dropdown for selecting the Wikidata property */}
-                <select onChange={handlePropertyChange}>
-                    {presetQueries.map((preset, index) => (
-                        <option key={index} value={preset.property}>
-                            {preset.label}
-                        </option>
-                    ))}
-                </select>
-                <button type="submit">Execute Preset Query</button>
-            </form>
-            {selectedQuery() === 'custom' && (
-                <form onSubmit={handleCustomQuerySubmit}>
-                    <input
-                        type="text"
-                        value={customQuery()}
-                        onInput={(e) => setCustomQuery(e.target.value)}
-                        placeholder="Enter custom SPARQL query"
-                    />
-                    <button type="submit">Execute Custom Query</button>
-                </form>
-            )}
-        </div>
-    );
+  const sampleQuery = `SELECT ?item ?itemLabel WHERE {
+    ?item wdt:${presetQueries[0].property} wd:${presetQueries[0].value};
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  } LIMIT 10`;
+
+  return (
+    <div>
+      <h2>SPARQL Query</h2>
+      <form onSubmit={handlePresetQuerySubmit}>
+        <select onChange={handleQueryChange}>
+          {availableQueryOptions}
+        </select>
+        <select onChange={handlePropertyChange}>
+          {presetQueries.map((preset, index) => (
+            <option key={index} value={preset.property}>
+              {preset.label}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Execute Preset Query</button>
+      </form>
+      <form onSubmit={handleCustomQuerySubmit}>
+        <textarea
+          value={customQuery()}
+          onInput={(e) => setCustomQuery(e.target.value)}
+          placeholder="SELECT ?item ?itemLabel WHERE { ?item wdt:P31 wd:Q5; SERVICE wikibase:label { bd:serviceParam wikibase:language '[AUTO_LANGUAGE],en'. }} LIMIT 2"
+          rows={5}
+          cols={40}
+        ></textarea>
+        <p></p>
+        <button type="submit">Execute Custom Query</button>
+        {loading() && <div>Loading...</div>} {/* Display loading icon if loading is true */}
+
+        
+      </form>
+    </div>
+  );
 };
