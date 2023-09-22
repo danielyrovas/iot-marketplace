d17cbe903587ae9966f9a56591514e4d6d08841d
Aldro <aldroaxeldes@gmail.com>
2023-09-22 23:42:44 +1000

    Function & UI Updates

--- src/pages/querySensor.jsx
+++ src/pages/querySensor.jsx
@@ -2,6 +2,43 @@ import { createSignal, createEffect } from "solid-js";
 import { QueryInput } from "../logic/QueryInput";
 
 export default function QuerySensor() {
+  const presetQueries = [
+    {
+      label: "Person",
+      property: "P31",
+      value: "Q5",
+    },
+    {
+      label: "City",
+      property: "P31",
+      value: "Q515",
+    },
+    {
+      label: "Country",
+      property: "P31",
+      value: "Q6256",
+    },
+  ];
+
+  const [selectedQuery, setSelectedQuery] = createSignal(0);
+  const [selectedProperty, setSelectedProperty] = createSignal(
+    presetQueries[0].value
+  );
+  const availableQueryOptions = [];
+  const handlePresetQuerySubmit = (event) => {
+    event.preventDefault();
+    const selectedPresetQuery = `SELECT ?item ?itemLabel WHERE {
+  ?item wdt:P31 wd:${selectedProperty()};
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+} LIMIT 10`;
+    fetchRdfData(selectedPresetQuery);
+  };
+
+  const handlePropertyChange = (event) => {
+    const selectedPropertyCode = event.target.value;
+    setSelectedProperty(selectedPropertyCode);
+    console.log(selectedPropertyCode);
+  };
   const datadummy = [
     {
       location: "Melbourne",
@@ -165,7 +202,38 @@ export default function QuerySensor() {
     <>
       <div className="mx-auto w-full max-w-[1200px] flex flex-row gap-4 min-h-screen">
         <div className="w-[300px] p-4 flex flex-col gap-4">
-          <button className="border rounded px-4 py-2" onClick={togglePopup}>
+          <form className="flex flex-col gap-4" onSubmit={handlePresetQuerySubmit}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">SPARQL Query</span>
+              </label>
+              <select
+                className="select select-bordered"
+                onChange={handlePropertyChange}
+              >
+                <option disabled selected>
+                  Pick one
+                </option>
+                {presetQueries.map((data) => (
+                  <option value={data.value}>{data.label}</option>
+                ))}
+              </select>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Custom Query</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+                disabled
+              />
+            </div>
+            <button className="btn">Execute Query</button>
+          </form>
+
+          {/* <button className="border rounded px-4 py-2" onClick={togglePopup}>
             Location
           </button>
           <button className="border rounded px-4 py-2" onClick={togglePopup2}>
@@ -173,34 +241,190 @@ export default function QuerySensor() {
           </button>
           <button className="border rounded px-4 py-2" onClick={togglePopup3}>
             Execute Query
-          </button>
+          </button> */}
         </div>
         <div className="grow p-4">
           <div className="grid grid-cols-4 w-full gap-4">
             {mapData() !== null
-              ? mapData().map((data) => {
+              ? mapData().map((data, index) => {
                   if (data.personLabel) {
                     return (
-                      <div className="p-4 border rounded bg-white">
-                        {data.personLabel.value}
+                      <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                          <h2 className="card-title">
+                            {data.personLabel.value}
+                          </h2>
+                          <p>
+                            If a dog chews shoes whose shoes does he choose?
+                          </p>
+                          <div className="card-actions justify-start">
+                            <button className="btn btn-primary">Details</button>
+                          </div>
+                        </div>
                       </div>
                     );
                   } else if (data.cityLabel) {
                     return (
-                      <div className="p-4 border rounded bg-white">
-                        {data.cityLabel.value}
+                      <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                          <h2 className="card-title">{data.cityLabel.value}</h2>
+                          <p>
+                            If a dog chews shoes whose shoes does he choose?
+                          </p>
+                          <div className="card-actions justify-start">
+                            <button className="btn btn-primary">Details</button>
+                          </div>
+                        </div>
                       </div>
                     );
                   } else if (data.countryLabel) {
                     return (
-                      <div className="p-4 border rounded bg-white">
-                        {data.countryLabel.value}
+                      <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                          <h2 className="card-title">
+                            {data.countryLabel.value}
+                          </h2>
+                          <p>
+                            If a dog chews shoes whose shoes does he choose?
+                          </p>
+                          <div className="card-actions justify-start">
+                            <button className="btn btn-primary">Details</button>
+                          </div>
+                        </div>
                       </div>
                     );
                   } else if (data.itemLabel) {
                     return (
-                      <div className="p-4 border rounded bg-white">
-                        {data.itemLabel.value}
+                      <div className="card w-full bg-base-100 shadow-xl">
+                        <div className="card-body">
+                          <h2 className="card-title">{data.itemLabel.value}</h2>
+                          <p>
+                            If a dog chews shoes whose shoes does he choose?
+                          </p>
+                          <div className="card-actions justify-start">
+                            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                            <button
+                              className="btn"
+                              onClick={() =>
+                                document
+                                  .getElementById(`modal-${index}`)
+                                  .showModal()
+                              }
+                            >
+                              Details
+                            </button>
+                            <dialog id={`modal-${index}`} className="modal">
+                              <div className="modal-box">
+                                <form method="dialog">
+                                  {/* if there is a button in form, it will close the modal */}
+                                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                                    ✕
+                                  </button>
+                                </form>
+                                <h3 className="font-bold text-lg">Details</h3>
+                                <div className="overflow-x-auto">
+                                  <table className="table">
+                                    {/* head */}
+                                    <thead>
+                                      <tr>
+                                        <th></th>
+                                        <th>Subject</th>
+                                        <th>Predicate</th>
+                                        <th>Object</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      {/* row 1 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Observes</td>
+                                        <td>SensorVideo</td>
+                                      </tr>
+                                      {/* row 2 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>FeatureOfInterest</td>
+                                        <td>CameraSensorLocation</td>
+                                      </tr>
+                                      {/* row 3 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Label</td>
+                                        <td>Video</td>
+                                      </tr>
+                                      {/* row 4 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Latitude</td>
+                                        <td>-37.821658</td>
+                                      </tr>
+                                      {/* row 5 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Longitude</td>
+                                        <td>145.03904</td>
+                                      </tr>
+                                      {/* row 6 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Altitude</td>
+                                        <td>12.75</td>
+                                      </tr>
+                                      {/* row 7 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Country</td>
+                                        <td>Australia</td>
+                                      </tr>
+                                      {/* row 6 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Provenance</td>
+                                        <td>Queensland</td>
+                                      </tr>
+                                      {/* row 7 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>City</td>
+                                        <td>Brisbane</td>
+                                      </tr>
+                                      {/* row 8 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Suburb</td>
+                                        <td>Stafford</td>
+                                      </tr>
+                                      {/* row 9 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Address</td>
+                                        <td>9 Webster Street</td>
+                                      </tr>
+                                      {/* row 10 */}
+                                      <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td>Postcode</td>
+                                        <td>4053</td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </div>
+                              </div>
+                            </dialog>
+                          </div>
+                        </div>
                       </div>
                     );
                   }
@@ -313,9 +537,7 @@ export default function QuerySensor() {
               <div>Execute Query</div>
               <button onClick={togglePopup3}>Close</button>
             </div>
-            <div className="flex flex-col gap-4">
-              <QueryInput executeQuery={fetchRdfData} />
-            </div>
+            <div className="flex flex-col gap-4"></div>
           </div>
         </div>
       )}
