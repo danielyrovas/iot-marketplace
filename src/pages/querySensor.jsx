c9154d7c6002254a93d87fd3a105c231d3f639ff
Daniel Yrovas <dan@danielyrovas.com>
2023-09-28 19:56:00 +1000

    wip

--- src/pages/querySensor.jsx
+++ src/pages/querySensor.jsx
@@ -9,7 +9,8 @@ export default function QuerySensor() {
   const [data, setData] = createSignal('');
   const [state, { updateConfig }] = useAppContext();
 
-  const fetchRdfData = async (query, isPresetQuery) => {
+  const fetchRdfData = async (query2, isPresetQuery) => {
+    let query = "SELECT ?sensor ?lat ?long ?measures WHERE { ?sensor <http://www.w3.org/ns/sosa/observes> ?observes. ?sensor <http://www.w3.org/ns/sosa/hasFeatureOfInterest> ?location. ?observes <http://www.w3.org/2000/01/rdf-schema#label> ?measures . ?location <http://www.w3.org/2003/01/geo/wgs84_pos#lat> ?lat .  ?location <http://www.w3.org/2003/01/geo/wgs84_pos#long> ?long . ?observes <http://www.w3.org/2000/01/rdf-schema#label> \"video\"}"
     try {
       let response = await fetch(`${state.api}/sparql`, {
         method: 'POST',
